55f4fa4b03cc79b66a0116d39e95fda4da47b7eb
chrisdiego <chrisdiego1@gmail.com>
2019-11-07 18:10:11 -0800

    resolving syntax error so this will run

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route, Link } from 'react-router-dom';
 import ActionTracker from './ActionTracker';
 import ActionDetail from './ActionDetail';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 import './ActionTracker.scss'
 
 const App = () => {
   return (
     <Container fluid>
-      <h1 className="bg-secondary text-white p-5 font-weight-bold shadow">SHMCAP Action Tracker</h1>
       <HashRouter>
+        <div className="bg-secondary p-5 shadow">
+          <h1 className="d-inline text-white font-weight-bold">SHMCAP Action Tracker</h1>
+          <Link to="/">
+            <FontAwesomeIcon className="text-white float-right" icon={faTimesCircle} size="4x" />
+          </Link>        
+        </div>
         <Route exact path="/">
             <ActionTracker />
         </Route>
