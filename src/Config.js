51004b23427ee665cc915f71938091ba4729c8b2
Cary FitzHugh <cary.fitzhugh@gmail.com>
2019-11-23 16:28:43 -0500

    updated the API

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,5 @@
 
 export default {
-  api_host: "http://localhost:5000",
-  //api_host: "http://ma-state-action-tracker.us-east-1.elasticbeanstalk.com",
+  //api_host: "http://localhost:5000",
+  api_host: "http://ma-state-action-tracker.us-east-1.elasticbeanstalk.com",
 };
